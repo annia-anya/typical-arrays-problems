11a50f315e151dc35811b5eae9d86da9fbb76a63
Hanna Shatsko <NytaShetko@yandex.ru>
2021-02-21 21:38:53 +0300

    init: Add min, max, avg implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
-exports.min = function min (array) {
-  return 0;
+function withCheckEmpty(fn) {
+    return (array) => {
+        if (!array || array.length === 0) {
+            return 0;
+        }
+        return fn(array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = withCheckEmpty(function(array) {
+  let currentMin = array[0];
+  for (let i = 1; i < array.length; i++) {
+      if(array[i] < currentMin){
+          currentMin = array[i];
+      }
+  }
+  return currentMin;
+});
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = withCheckEmpty(function(array) {
+    let currentMax = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if(array[i] > currentMax){
+            currentMax = array[i];
+        }
+    }
+    return currentMax;
+  })
+
+exports.avg = withCheckEmpty(function(array) {
+  const length = array.length;
+  let sum = 0;
+//   let sum = array.reduce((result, item) => result + item, 0);
+
+  for(let i = 0; i < array.length; i++) {
+      sum += array[i];
+  }
+  return sum / length;
+  // return array.reduce((result, item) => result + item, 0) / array.length;
+})
